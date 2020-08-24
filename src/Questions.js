c1ff929b856540a636b33445225870194af1bf49
sauravk7077 <sauravk7077@gmail.com>
2020-08-24 11:10:22 +0530

    added questions

--- src/Questions.js
+++ src/Questions.js
@@ -1,22 +1,32 @@
 export const questions = [
     {
-        question: "Which is your favourite toffee brand?",
-        options: ["Nestle", "Cadbury"],
-        answer: "Nestle"
+        question: "Who is the first person to reach North Pole?",
+        options: ["Robert Peary", "Rajesh Sharma", "Charles Hillary","Johan don"],
+        answer: "Robert Peary"
     },
     {
-        question: "Which is your favourite drink?",
-        options: ["Tea", "Cofee"],
-        answer: "Tea"
+        question: "Who is the first person to reach the South Pole?",
+        options: ["Amundsen", "Rajesh Sharma","Charles Hillary","Johan don"],
+        answer: "Amundsen"
     },
     {
-        question: "Which is your favourite food?",
-        options: ["Cake", "Yogurt", "CheeseCake", "Paneer"],
-        answer: "Paneer"
+        question: "Which is the first country to issue paper currency?",
+        options: ["China", "India", "USA", "UK"],
+        answer: "China"
     },
     {
-        question: "Which is your favourite toffee brand?",
-        options: ["Nestle", "Cadbury"],
-        answer: "Nestle"
+        question: "Who is the  first Governor General of the United Nations?",
+        options: ["Trigveli (Norway)", "Robert Walpole", "Henry Waterloo", "George Bush"],
+        answer: "Trigveli (Norway)"
+    },
+    {
+        question: " Which is the first country to host NAM summit?",
+        options: ["Belgrade (Yugoslavia)", "India", "USA", "UK"],
+        answer: "Belgrade (Yugoslavia)"
+    },
+    {
+        question: "Who was the first person to sail round the world?",
+        options: ["Magellan", "Robert Walpole", "Henry Waterloo", "George Bush"],
+        answer: "Magellan"
     },
 ]
