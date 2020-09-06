62eb11f356f34769101d85a136aa90df715dbf1d
sauravk7077 <sauravk7077@gmail.com>
2020-09-06 08:24:29 +0530

    pages

---
+++ precache-manifest.3d29a5f0e3197b7a33df7c67ab8b6035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a807d6de0825e83f14d82c6c328961cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1d3969caabe3e8c4076",
+    "url": "/static/css/main.6fbf533f.chunk.css"
+  },
+  {
+    "revision": "a7cd7ee1716ee98e9629",
+    "url": "/static/js/2.8df378cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8df378cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d3969caabe3e8c4076",
+    "url": "/static/js/main.4d57aa9d.chunk.js"
+  },
+  {
+    "revision": "06e76c23334e4ae759d3",
+    "url": "/static/js/runtime-main.8abfe3bd.js"
+  }
+]);
