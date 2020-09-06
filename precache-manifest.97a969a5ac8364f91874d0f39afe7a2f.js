15b96f1b82459955a2696692f226385dc8793566
sauravk7077 <sauravk7077@gmail.com>
2020-09-06 08:45:28 +0530

    homepage change

---
+++ precache-manifest.97a969a5ac8364f91874d0f39afe7a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1d035fae3e0bce6748bf2ca1ee0a8a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1fc831ee090bb9e01640",
+    "url": "./static/css/main.0c61f48d.chunk.css"
+  },
+  {
+    "revision": "a7cd7ee1716ee98e9629",
+    "url": "./static/js/2.8df378cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8df378cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc831ee090bb9e01640",
+    "url": "./static/js/main.b7666e8c.chunk.js"
+  },
+  {
+    "revision": "a31b48e610514585c5b0",
+    "url": "./static/js/runtime-main.765c5fd3.js"
+  }
+]);
