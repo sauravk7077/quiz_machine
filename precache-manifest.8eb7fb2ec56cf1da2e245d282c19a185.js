e7da209f56450a61b95e773d001a51160e00cde9
sauravk7077 <sauravk7077@gmail.com>
2020-09-07 07:30:35 +0530

    responsive ui

---
+++ precache-manifest.8eb7fb2ec56cf1da2e245d282c19a185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c46527bf22aa76a79cf81cc75b641e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "994b338bc4de201aff3d",
+    "url": "./static/css/main.f13ff5ef.chunk.css"
+  },
+  {
+    "revision": "a7cd7ee1716ee98e9629",
+    "url": "./static/js/2.8df378cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8df378cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "994b338bc4de201aff3d",
+    "url": "./static/js/main.186ae4aa.chunk.js"
+  },
+  {
+    "revision": "a31b48e610514585c5b0",
+    "url": "./static/js/runtime-main.765c5fd3.js"
+  }
+]);
